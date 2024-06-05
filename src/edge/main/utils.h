48c436ba2f2dc3e568aa63ad694770ef0d955316
Hackfront <hackfront@protonmail.ch>
2024-06-05 11:47:15 +0200

    Add base for edge server

---
+++ src/edge/main/utils.h
@@ -0,0 +1,10 @@
+#ifndef N_UTILS_H
+#define N_UTILS_H
+
+#define C_ABS(a,b)      ((a * a) + (b * b))
+#define C_DELAY_MS(v)   ((v) / portTICK_PERIOD_MS)
+#define C_LIN_TO_DB(l)  (10 * log10f(l))
+
+#define S_ESCAPE(...)   #__VA_ARGS__
+
+#endif /* end of include guard: N_UTILS_H */
