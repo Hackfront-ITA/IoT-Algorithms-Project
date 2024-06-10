442dc69effcf953ff2bb1e490434d1def126fe02
Hackfront <hackfront@protonmail.ch>
2024-06-11 01:33:22 +0200

    Add some math functions to node utils

--- src/node/main/utils.c
+++ src/node/main/utils.c
@@ -1,3 +1,4 @@
+#include <math.h>
 #include <stdint.h>
 
 #include <esp_err.h>
@@ -18,4 +19,46 @@ uint16_t gen_dev_id(void) {
 	dev_id = buf[0] + buf[1] + buf[2];
 
 	return dev_id;
+}
+
+float calc_mean(float *data, size_t len) {
+	float mean = 0.0;
+
+	for (size_t i = 0; i < len; i++) {
+		mean += data[i];
+	}
+
+	mean /= len;
+
+	return mean;
+}
+
+void calc_z_score(float *input, float *output, size_t len) {
+	float mean = 0;
+	for (size_t i = 0; i < len; i++) {
+		mean += input[i];
+	}
+	mean /= len;
+
+	float variance = 0;
+	for (size_t i = 0; i < len; i++) {
+		variance += pow(input[i] - mean, 2);
+	}
+	variance /= len;
+
+	// Standard deviation
+	float std_dev = sqrt(variance);
+
+	// Compute z score for each frequency
+	for (size_t i = 0; i < len; i++) {
+		output[i] = (input[i] - mean) / std_dev;
+	}
+}
+
+void remove_mean(float *data, size_t len) {
+	float mean = calc_mean(data, len);
+
+	for (size_t i = 0; i < len; i++) {
+		data[i] -= mean;
+	}
 }
--- src/node/main/utils.h
+++ src/node/main/utils.h
@@ -1,6 +1,8 @@
 #ifndef N_UTILS_H
 #define N_UTILS_H
 
+#include <stdint.h>
+
 #define C_ABS(a,b)      ((a * a) + (b * b))
 #define C_DELAY_MS(v)   ((v) / portTICK_PERIOD_MS)
 #define C_LIN_TO_DB(l)  (10 * log10f(l))
@@ -8,5 +10,9 @@
 #define S_ESCAPE(...)   #__VA_ARGS__
 
 uint16_t gen_dev_id(void);
+
+float calc_mean(float *data, size_t len);
+void calc_z_score(float *input, float *output, size_t len);
+void remove_mean(float *data, size_t len);
 
 #endif /* end of include guard: N_UTILS_H */
