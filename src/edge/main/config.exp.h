24b12066f50cbcd828165db41dcfdf80d7a52c9f
Hackfront <hackfront@protonmail.ch>
2024-06-12 16:31:14 +0200

    Add local config.h files

---
+++ src/edge/main/config.exp.h
@@ -0,0 +1,13 @@
+#ifndef E_CONFIG_H
+#define E_CONFIG_H
+
+#define NET_WIFI_SSID       "<ssid>"
+#define NET_WIFI_PASSWORD   "<password>"
+#define NET_WIFI_SECURITY   WIFI_AUTH_WPA_WPA2_PSK
+
+#define MQTT_BROKER_URL  "mqtts://192.168.1.23:8883"
+
+#define BUILDING_ID     8443
+#define TIMESYNC_DELAY  (20 * 1000)
+
+#endif /* end of include guard: E_CONFIG_H */
---
+++ src/node/main/config.exp.h
@@ -0,0 +1,9 @@
+#ifndef A_CONFIG_H
+#define A_CONFIG_H
+
+#define DATA_SAMPLING_FREQ    200
+#define DATA_NUM_SAMPLES      2048
+
+#define EDGE_DEV_ID   0x5fb6
+
+#endif /* end of include guard: A_CONFIG_H */
